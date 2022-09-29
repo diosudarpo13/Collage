d0cdd62882322c26a95c43c8517ff0fca32e6aa0
diosudarpo13 <45312920+diosudarpo13@users.noreply.github.com>
2022-09-29 20:41:31 +0700

    Add files via upload

---
+++ 19220302 Tugas Mandiri 1.py
@@ -0,0 +1,11 @@
+# Diketahui
+brt = 5
+hrg = 26000
+ongkos = 3500
+uang = 200000
+# Perhitungan
+telur = hrg*brt
+angkot = ongkos*2
+sisa = uang-telur-angkot
+# Cetak
+print("Sisa dari uang ibu adalah:", sisa)
---
+++ 19220302 Tugas Mandiri 2.py
@@ -0,0 +1,4 @@
+hrg = int(input('Harga Per KG:'))
+brt = int(input('Berat Pembelian (KG):'))
+byr = hrg*brt
+print('Harga yang perlu dibayar =', byr)
